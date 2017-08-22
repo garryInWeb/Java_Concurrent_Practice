da7cc202fdf7f72dd474431fb10f34e9a384528f
garryInWeb <987357718@qq.com>
2017-08-22 18:46:09 +0800

    add factory

---
+++ src/com/thread/create/ThreadCreateFactory.java
@@ -0,0 +1,8 @@
+package com.thread.create;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadCreateFactory {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+}
